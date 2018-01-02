a47127b9492388553e5ae6b1bd416e1ca6dbdae0
Stewart ilondanga <stilondanga@gmail.com>
2018-01-02 16:27:47 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,26 @@ function sendMessage(msg){
   updateUI(msg);
 }
 
+function updateUI(data){
+  var wrap = document.createElement('li');
+  wrap.className = "me";
+  var title = document.createElement('b');
+  var name = document.createElement('span');
+  var avatar = document.createElement('img');
+  var content = document.createElement('span');
+  name.innerHTML = data.user.screen_name + " ";
+  avatar.src = data.user.avatar;
+  content.innerHTML = data.text;
+  content.className = "text";
+  avatar.className = "ava";
+  title.appendChild(name);
+  title.appendChild(avatar);
+  wrap.appendChild(title);
+  wrap.appendChild(content);
+  $('chat-list').appendChild(wrap);
+  $('text').value="";
+}
+
 
 
 var navigate = (function() {
